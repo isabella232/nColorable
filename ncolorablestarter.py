0ca1c3c6ff758127af4bc649cb8e3e73491f0ca7
JasonL24 <32722492+JasonL24@users.noreply.github.com>
2019-11-02 16:34:15 -0700

    Update ncolorablestarter.py

--- ncolorablestarter.py
+++ ncolorablestarter.py
@@ -3,17 +3,22 @@
 
 import csv
 
+
 def checkVertex(graph, curIndex, colors, color, numVertices):
     # Function checks if it's okay to assign color to this vertex
+
 
 def graphColoring(graph, n, colors, curIndex, numVertices):
     # This function will handle the coloring of the nodes. It will require you to use DFS and backtracking search concepts.
 
+    
 def initColors(numVertices):
     # Initialize the colors vector and return it
 
-def fileReader(filename, numVertices):
+    
+def fileReader(filename):
     # Read the content from the CSV file into a 2D list
 
+    
 def main():
     # Get user input and call the appropriate functions in here
