05b4b5f1344a0bf6156fed7e93d53848e1c162a3
JasonL24 <32722492+JasonL24@users.noreply.github.com>
2019-11-02 12:20:45 -0700

    Add files via upload

---
+++ ncolorablestarter.py
@@ -0,0 +1,19 @@
+# Undirected graph is stored in a adjacency matrix with 1 if graph goes from index i to j
+# The program should output an array called colors that goes from 1 to n. colors[i] returns the color from 1 to n assigned to vertex i
+
+import csv
+
+def checkVertex(graph, curIndex, colors, color, numVertices):
+    # Function checks if it's okay to assign color to this vertex
+
+def graphColoring(graph, n, colors, curIndex, numVertices):
+    # This function will handle the coloring of the nodes. It will require you to use DFS and backtracking search concepts.
+
+def initColors(numVertices):
+    # Initialize the colors vector and return it
+
+def fileReader(filename, numVertices):
+    # Read the content from the CSV file into a 2D list
+
+def main():
+    # Get user input and call the appropriate functions in here
